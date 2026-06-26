355d9801763d107e5e1295a6870ea2e1caf63594
yahyapqb <yhyyhmldar8@gmail.com>
2026-06-26 06:39:22 -0700

    input added

--- key_generator.py
+++ key_generator.py
@@ -2,6 +2,7 @@ import string
 import random
 password = ""
 b = 0
+c = int(input("تعداد رمز مورد نیازتو وارد کن: " ))
 while True:
     for i in range(1):
         num = random.choice(string.digits)
@@ -11,7 +12,7 @@ while True:
         a = random.choice([num,sma,bi])
         b = b + 1
         password += a
-    if (b == 8):
+    if (b == c):
         break
 print(password)
 
