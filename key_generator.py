54791c6a432ce0d10260a2b5d7ae58e5df675b56
yahyapqb <yhyyhmldar8@gmail.com>
2026-06-26 08:46:53 -0700

    new

--- key_generator.py
+++ key_generator.py
@@ -1,18 +1,18 @@
 import string
 import random
 password = ""
-b = 0
-c = int(input("تعداد رمز مورد نیازتو وارد کن: " ))
+stop_time = 0
+password_digits = int(input("please Enter the number of digits in the password.: " ))
 while True:
     for i in range(1):
         num = random.choice(string.digits)
-        sma = random.choice(string.ascii_lowercase)
-        bi = random.choice(string.ascii_uppercase)
+        small = random.choice(string.ascii_lowercase)
+        big = random.choice(string.ascii_uppercase)
     for i in range(1):
-        a = random.choice([num,sma,bi])
-        b = b + 1
-        password += a
-    if (b == c):
+        word_or_number = random.choice([num,small,big])
+        stop_time = stop_time + 1
+        password += word_or_number
+    if (stop_time == password_digits):
         break
 print(password)
 print("password created")
